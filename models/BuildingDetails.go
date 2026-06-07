74101ca7393978d967fdf8d2120bf7db7d32be3a
Pandas-1 <khemkavarun11@gmail.com>
2026-06-08 02:39:37 +0530

    feat: added BuildingDetails model

--- models/BuildingDetails.go
+++ models/BuildingDetails.go
@@ -1,9 +1,15 @@
 package models
 
-type Buiding struct {
+import(
+	"rats/db"
+	"github.com/lib/pq"
+
+)
+
+type BuildingDetails struct {
 	BuildingID int
 	Name string
-	Typeint char
+	BuildingType string
 	Production int
 	Scaling int
 	HealthBar int
@@ -11,8 +17,39 @@ type Buiding struct {
 	Height int
 	DefenceAttack int
 	DefenceRange int
-	MaxLevel []int
+	MaxLevel pq.Int64Array
 	CostResource1 int
 	CostResource2 int
 
+}
+
+func GetBuildingDetails(buildingID int) (BuildingDetails, error) {
+	var b BuildingDetails
+
+	row := db.DB.QueryRow(
+		`SELECT building_id, name, building_type, production, scaling,
+		        health_bar, width, height, defence_attack,
+		        defence_range, max_level, cost_resource1, cost_resource2
+		   FROM building_details
+		  WHERE building_id = $1`,
+		buildingID,
+	)
+
+	err := row.Scan(
+		&b.BuildingID,
+		&b.Name,
+		&b.BuildingType,
+		&b.Production,
+		&b.Scaling,
+		&b.HealthBar,
+		&b.Width,
+		&b.Height,
+		&b.DefenceAttack,
+		&b.DefenceRange,
+		&b.MaxLevel,
+		&b.CostResource1,
+		&b.CostResource2,
+	)
+
+	return b, err
 }
