48e59483201e90c2b2b8de6d79b76eefc7b2571f
Pandas-1 <khemkavarun11@gmail.com>
2026-06-07 17:55:18 +0530

    model functions for CityDetails

--- models/CityDetails.go
+++ models/CityDetails.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"time"
+	"rats/db"
+)
 
 type CityDetails struct {
 	UserID int
@@ -12,4 +15,33 @@ type CityDetails struct {
 	LastUpdated time.Time
 	MaxDefenceBuildings int 
 	MaxResourceBuildings int
+}
+
+
+func CreateCity(userID int) error {
+	_ , err := db.DB.Exec(
+		"INSERT into city_details (user_id, resource1 , resource2, max_resource1 , max_resource2, max_troop_army_size, last_updated, max_defence_buildings, max_resource_buildings) VALUES ($1, 100 , 100 , 1000, 1000, 30, NOW(), 4, 4) ", 
+		userID,
+	)
+	return err
+} 
+
+func GetCity(userID int) (CityDetails, error) {
+	var c CityDetails
+	c.UserID = userID
+	row := db.DB.QueryRow(
+		"SELECT resource1,resource2,max_resource1,max_resource2,max_troop_army_size,last_updated,max_defence_buildings, max_resource_buildings FROM city_details WHERE user_id = $1",
+		userID,
+	)
+	err := row.Scan(&c.Resource1, &c.Resource2, &c.MaxResource1, &c.MaxResource2, &c.MaxTroopArmySize, &c.LastUpdated, &c.MaxDefenceBuildings, &c.MaxResourceBuildings)
+    return c, err
+}
+
+func UpdateResources(userID int, resource1, resource2 int64) error {
+	_ , err := db.DB.Exec(
+		"UPDATE city_details SET resource1 = $2, resource2 = $3 WHERE user_id = $1",
+		userID, resource1, resource2,
+	)
+	return err
+
 }
