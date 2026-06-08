15835374e262adfc1505e236bd3e1cdc50f49a89
Pandas-1 <khemkavarun11@gmail.com>
2026-06-08 18:27:09 +0530

    feat: Move Building with collision check

--- models/UserBuilding.go
+++ models/UserBuilding.go
@@ -88,3 +88,40 @@ func GetVillageBuildings(userID int) ([]UserBuilding, error) {
 
     return buildings, nil
 }
+
+func MoveBuilding(InstanceID int, x int, y int) error {
+    var width, height, userID int
+    err := db.DB.QueryRow(
+        `SELECT bd.width, bd.height, ub.user_id 
+        FROM user_buildings ub
+        JOIN building_details bd ON ub.building_id = bd.building_id
+        WHERE ub.instance_id = $1`,
+        InstanceID,
+    ).Scan(&width, &height, &userID)
+    // i gotta remove the orignal building when doing collision
+    if err != nil {
+        return err
+    }
+    var count int
+    err = db.DB.QueryRow(
+        `SELECT COUNT(*) FROM user_buildings ub
+        JOIN building_details bd ON ub.building_id = bd.building_id
+        WHERE ub.user_id = $1
+        AND ub.grid_x < $2 AND ub.grid_x + bd.width > $3
+        AND ub.grid_y < $4 AND ub.grid_y + bd.height > $5
+        AND ub.instance_id != $6`,
+        userID, x+width, x, y+height, y, InstanceID,
+    ).Scan(&count)
+    if err != nil {
+        return err
+    }
+    if count > 0 {
+        return fmt.Errorf("grid space already occupied")
+    }
+
+    _, err = db.DB.Exec(
+        "UPDATE user_buildings SET grid_x = $1 , grid_y = $2 WHERE instance_id = $3 ",
+        x, y, InstanceID,
+    )
+    return err
+}
