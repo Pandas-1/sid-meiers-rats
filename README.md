54feb2c0c29196da200c9f78460b2bf28799cf5a
Pandas-1 <khemkavarun11@gmail.com>
2026-06-08 22:58:11 +0530

    feat: added function to call for Troop details

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sid-meiers-rats
+
+
+8-6-2026 - Working on models, almost done with all elemetry models. Will add as i need them afterwards
--- models/TroopDetails.go
+++ models/TroopDetails.go
@@ -1,16 +1,52 @@
 package models
 
+import (
+    "rats/db"
+    "github.com/lib/pq"
+)
+
 type Troop struct {
-	TroopID int
-	Name string
-	BaseCost int
-	TroopAttackPower int
-	BuildingAttackPower int
-	Defence int
-	Range int
-	AttributeStrength int
-	AttributeWeakness int
-	TroopArmySpace int
-	MovementSpeed int
-	MaxLevel []int
+    TroopID             int
+    Name                string
+    BaseCost            int
+    TroopAttackPower    int
+    BuildingAttackPower int
+    Defence             int
+    Range               int
+    AttributeStrength   int
+    AttributeWeakness   int
+    TroopArmySpace      int
+    MovementSpeed       int
+    MaxLevel            pq.Int64Array
+}
+
+
+func GetTroopDetails(troopID int) (Troop, error) {
+    var t Troop
+
+    row := db.DB.QueryRow(
+        `SELECT troop_id, name, base_cost, troop_attack_power, building_attack_power, 
+                defence, range, attribute_strength, attribute_weakness, 
+                troop_army_space, movement_speed, max_level
+           FROM troop_details
+          WHERE troop_id = $1`,
+        troopID,
+    )
+
+    err := row.Scan(
+        &t.TroopID,
+        &t.Name,
+        &t.BaseCost,
+        &t.TroopAttackPower,
+        &t.BuildingAttackPower,
+        &t.Defence,
+        &t.Range,
+        &t.AttributeStrength,
+        &t.AttributeWeakness,
+        &t.TroopArmySpace,
+        &t.MovementSpeed,
+        &t.MaxLevel,
+    )
+
+    return t, err
 }
