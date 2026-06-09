f76c73919aedbc7a9603367e20e7846fe611c204
Pandas-1 <khemkavarun11@gmail.com>
2026-06-10 02:37:09 +0530

    refactor: CreateUser now makes the db entries for all the tables where required

--- models/Users.go
+++ models/Users.go
@@ -3,6 +3,7 @@ package models
 import (
 	"time"
 	"rats/db"
+	"fmt"
 )
 
 type User struct {
@@ -16,11 +17,61 @@ type User struct {
 }
 
 func CreateUser(username, passwordHash string) error {
-	_, err := db.DB.Exec(
-        "INSERT INTO users (username, password_hash, trophies, base_level, xp, last_played) VALUES ($1, $2, 0, 1, 0, NOW())",
+    // start transaction
+    tx, err := db.DB.Begin()
+    if err != nil {
+        return err
+    }
+    defer tx.Rollback()
+
+    var userID int
+    err = tx.QueryRow(
+        "INSERT INTO users (username, password_hash, base_level, xp, last_played) VALUES ($1, $2, 1, 0, NOW()) RETURNING user_id",
         username, passwordHash,
+    ).Scan(&userID)
+    if err != nil {
+        return fmt.Errorf("could not create user: %w", err)
+    }
+
+    //create city_details entry, bcz i am fucking with transactions idk if i can call the CreateCity but i dont really care i think
+    _, err = tx.Exec(
+        "INSERT INTO city_details (user_id, resource1, resource2, max_resource1, max_resource2, max_troop_army_size, last_updated, max_defence_buildings, max_resource_buildings) VALUES ($1, 100, 100, 1000, 1000, 30, NOW(), 4, 4)",
+        userID,
     )
-    return err
+    if err != nil {
+        return fmt.Errorf("could not create city: %w", err)
+    }
+
+    //create user_battle_history entry
+    _, err = tx.Exec(
+        "INSERT INTO user_battle_history (user_id, number_of_battles, battles_won, battles_lost, trophies) VALUES ($1, 0, 0, 0, 0)",
+        userID,
+    )
+    if err != nil {
+        return fmt.Errorf("could not create battle history: %w", err)
+    }
+
+    //reate army_details entry
+    _, err = tx.Exec(
+        "INSERT INTO army_details (user_id, troop_units_used, army_composition, created_on) VALUES ($1, 0, '[]', NOW())",
+        userID,
+    )
+    if err != nil {
+        return fmt.Errorf("could not create army: %w", err)
+    }
+
+    // create user_troop_details for ALL troops that exist in troop_details
+    _, err = tx.Exec(
+        `INSERT INTO user_troop_details (user_id, troop_id, troop_level)
+         SELECT $1, troop_id, 1 FROM troop_details`,
+        userID,
+    )
+    if err != nil {
+        return fmt.Errorf("could not create troop details: %w", err)
+    }
+
+    // all steps succeeded, commit
+    return tx.Commit()
 }
 
 func GetUserByUsername(username string) (User, error) {
